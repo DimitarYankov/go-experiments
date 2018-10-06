921bcc949e3e8f836c76934d9791f3d66e9f6786
dimitar.yankov <dimitar.yankov@dnhsoft.com>
2018-10-06 15:06:33 +0300

    Reformat code

--- cmd/go-experiments/main.go
+++ cmd/go-experiments/main.go
@@ -1,88 +1,98 @@
 package main
 
 import (
-  "os"
-  "log"
-  "fmt"
-  "time"
-  "context"
-  "net/http"
-  "github.com/gorilla/mux"
-  "github.com/DimitarYankov/go-experiments/internal/diagnostics"
+	"context"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/DimitarYankov/go-experiments/internal/diagnostics"
+	"github.com/gorilla/mux"
 )
 
-type serverConf struct{
-  port string
-  router http.Handler
-  name string
+type serverConf struct {
+	name   string
+	port   string
+	router http.Handler
 }
 
 func main() {
-   log.Print("Starting the application... ")
+	log.Print("Starting the application...")
 
-   blPort := os.Getenv("PORT")
-   if (len(blPort) == 0) {
-     log.Fatal("The app port should be set")
-   }
-   router := mux.NewRouter()
-   router.HandleFunc("/", hello)
+	blPort := os.Getenv("PORT")
+	if len(blPort) == 0 {
+		log.Fatal("The application port should be set")
+	}
 
-   diagnosticPort := os.Getenv("DIAG_PORT")
-   if (len(diagnosticPort) == 0) {
-     log.Fatal("The daignostic port should be set")
-   }
-   diagnostics := diagnostics.NewDiagnostics()
+	diagPort := os.Getenv("DIAG_PORT")
+	if len(diagPort) == 0 {
+		log.Fatal("The diagnostics port should be set")
+	}
 
-   possibleErrors := make(chan error, 2)
-   configurations := []serverConf{
-     {
-       port: blPort,
-       router: router,
-       name: "application server",
-     },
-     {
-       port: diagnosticPort,
-       router: diagnostics,
-       name: "diagnostic server",
-     },
-   }
+	router := mux.NewRouter()
+	router.HandleFunc("/", hello)
 
-   servers := make([]*http.Server, 2)
+	diagnostics := diagnostics.NewDiagnostics()
 
-   for i, c := range configurations {
-     go func (conf serverConf, i int) {
-       log.Printf("The %s is preparing to handle connections...", conf.name)
-       servers[i] = &http.Server{
-         Addr: ":"+conf.port,
-         Handler: conf.router,
-       }
-       err := servers[i].ListenAndServe()
-       if (err != nil) {
-         possibleErrors <- err
-       }
-     }(c, i)
-   }
+	possibleErrors := make(chan error, 2)
 
-   select {
-   case err := <-possibleErrors:
-     for _, s := range servers {
-       timeout := 5 * time.Second
-       log.Printf("\nShutdown with timeout: %s\n", timeout)
-       ctx, cancel := context.WithTimeout(context.Background(), timeout)
-       defer cancel()
-       customError := s.Shutdown(ctx)
-       if customError != nil {
-         fmt.Println(customError)
-       }
-       log.Printf("Server gracefully stopped")
-     }
-     log.Fatal(err)
+	configurations := []serverConf{
+		{
+			port:   blPort,
+			router: router,
+			name:   "application server",
+		},
+		{
+			port:   diagPort,
+			router: diagnostics,
+			name:   "diagnostics server",
+		},
+	}
 
-   }
+	servers := make([]*http.Server, 2)
 
+	for i, c := range configurations {
+		go func(conf serverConf, i int) {
+			log.Printf("The %s is preparing to handle connections...", conf.name)
+			servers[i] = &http.Server{
+				Addr:    ":" + conf.port,
+				Handler: conf.router,
+			}
+			err := servers[i].ListenAndServe()
+			if err != nil {
+				possibleErrors <- err
+			}
+		}(c, i)
+	}
+
+	interrupt := make(chan os.Signal, 1)
+	signal.Notify(interrupt, os.Interrupt, syscall.SIGTERM)
+
+	select {
+	case err := <-possibleErrors:
+		log.Printf("Got an error: %v", err)
+	case sig := <-interrupt:
+		log.Printf("Recevied the signal %v", sig)
+	}
+
+	for _, s := range servers {
+		timeout := 5 * time.Second
+		log.Printf("Shutdown with timeout: %s", timeout)
+		ctx, cancel := context.WithTimeout(context.Background(), timeout)
+		defer cancel()
+		err := s.Shutdown(ctx)
+		if err != nil {
+			fmt.Println(err)
+		}
+		log.Printf("Server gracefully stopped")
+	}
 }
 
 func hello(w http.ResponseWriter, r *http.Request) {
-  log.Print("The hello handler was called")
-  fmt.Fprint(w, http.StatusText(http.StatusOK))
+	log.Printf("The hello handler was called")
+	fmt.Fprint(w, http.StatusText(http.StatusOK))
 }
