87506864be3758399f3d69941454762e8b9d0fd3
dimitar.yankov <dimitar.yankov@dnhsoft.com>
2018-10-06 12:08:07 +0300

    Add env config for app and diagnostic port

--- cmd/go-experiments/main.go
+++ cmd/go-experiments/main.go
@@ -4,26 +4,35 @@ import (
   "log"
   "fmt"
   "net/http"
+  "os"
 
   "github.com/gorilla/mux"
   "github.com/DimitarYankov/go-experiments/internal/diagnostics"
 )
 
 func main() {
-   log.Print("Hello, World")
+   log.Print("Starting the application... ")
 
+   blPort := os.Getenv("PORT")
+   if (len(blPort) == 0) {
+     log.Fatal("The app port should be set")
+   }
+   daignosticPort := os.Getenv("DIAG_PORT")
+   if (len(daignosticPort) == 0) {
+     log.Fatal("The daignostic port should be set")
+   }
    router := mux.NewRouter()
    router.HandleFunc("/", hello)
 
    go func () {
-     err := http.ListenAndServe(":8080", router)
+     err := http.ListenAndServe(":"+blPort, router)
      if err != nil {
        log.Fatal(err)
      }
    }()
 
    diagnostics := diagnostics.NewDiagnostics()
-   err := http.ListenAndServe(":8585", diagnostics)
+   err := http.ListenAndServe(":"+daignosticPort, diagnostics)
    if err != nil {
      log.Fatal(err)
    }
